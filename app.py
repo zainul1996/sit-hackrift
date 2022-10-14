d2edafa25d1c1431dfbca40a6fdab54bbd2a4c14
ZainulCain <zainulcain@ZainulCains-MacBook-Pro.local>
2022-10-15 00:21:41 +0800

    added pwa support

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -7,5 +7,16 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+# routes for pwa
+@app.route("/manifest.webmanifest")
+def manifest():
+	return send_from_directory("static", "manifest.json")
+
+@app.route("/sw.js")
+def service_worker():
+	return send_from_directory("static", "sw.js")
+
+
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, port=8080)
