86beeda1eae8d1740da1575aea06ad60cb977402
Kok Fong <limkf97@gmail.com>
2022-10-15 01:00:35 +0800

    database linked

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, send_from_directory
+from dotenv import load_dotenv
+from pathlib import Path
+import pymongo
+import os
 
 app = Flask(__name__)
+
+dotenv_path = Path('credentials.env')
+load_dotenv(dotenv_path=dotenv_path)
+password = os.getenv('PASSWORD')
+client = pymongo.MongoClient("mongodb+srv://rushhour:"+password+"@cluster0.ksdhfcl.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+collection = db.test_collection
 
 @app.route("/")
 @app.route("/index")
