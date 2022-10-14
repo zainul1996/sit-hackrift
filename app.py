474614bb21ef4d2a8580714512c2bb3595ed462c
weiy343 <2508496Y@student.gla.ac.uk>
2022-10-15 02:55:09 +0800

    Add init mm

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
+
+temp = {}
 
 @app.route("/")
 @app.route("/index")
@@ -16,7 +18,11 @@ def manifest():
 def service_worker():
 	return send_from_directory("static", "sw.js")
 
-
+@app.route("/test", methods = ['POST'])
+def test():
+	temp = request.get_json()
+	print(temp)
+	return "1"
 
 if __name__ == '__main__':
 	app.run(debug=True, port=8080)
